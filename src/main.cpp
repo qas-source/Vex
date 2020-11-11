d1f2321fc56386be7196b586804189fae46f264c
qas-source <67299186+qas-source@users.noreply.github.com>
2020-11-11 17:39:50 -0500

    Init

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       qasimebsim                                                */
+/*    Created:      Sun Oct 18 2020                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Drivetrain           drivetrain    1, 2, 3, 4      
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+
+void spin () {
+
+  Brain.Screen.print("hello");
+}
+
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  // Drivetrain.driveFor(forward, 13, mm);
+  while (true) {
+    Controller1.ButtonDown.pressed(spin);
+    wait(0.05, seconds);
+  }
+  
+}
